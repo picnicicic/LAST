f476cfd27a9e5820d4a7fb1920c46f1396bf3580
picnicicic <dsd5384@naver.com>
2023-04-05 15:56:57 +0900

    LAST

--- app/src/main/java/com/example/last/MainActivity.kt
+++ app/src/main/java/com/example/last/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.last
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //내비게이션 컨트롤러
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+
+        //앱바 설정 객체
+        appBarConfiguration = AppBarConfiguration(setOf(R.id.homeFragment, R.id.profileFragment, R.id.settingFragment))
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        val bottomNav: BottomNavigationView = findViewById(R.id.bottomNav)
+
+        bottomNav.setupWithNavController(navController)
     }
 }
